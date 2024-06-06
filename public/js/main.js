c7fd0c9eab8abd7ba4ea452ad5b4348be7b74d36
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-06-06 21:12:24 +0200

    p

--- public/js/main.js
+++ public/js/main.js
@@ -2,43 +2,17 @@ const buttons = document.querySelectorAll('.navigation .menu button');
 const dropdowns = document.querySelectorAll('.dropdown');
 
 buttons.forEach(button => {
-  button.addEventListener('mouseover', () => {
+  button.addEventListener('click', () => {
     const id = button.id;
     const dropdown = document.getElementById(`${id}-dropdown`);
 
-    dropdown.classList.add('show');
-    button.classList.add('active');
-  });
-
-  button.addEventListener('mouseout', () => {
-    const id = button.id;
-    const dropdown = document.getElementById(`${id}-dropdown`);
-
-    dropdown.addEventListener('mouseover', () => {
-      dropdown.classList.add('show');
-      button.classList.add('active');
-    });
-
-    dropdown.addEventListener('mouseout', () => {
-      dropdown.classList.remove('show');
-      button.classList.remove('active');
-    });
-
-    dropdown.classList.remove('show');
-    button.classList.remove('active');
+    dropdown.classList.toggle('show');
+    button.classList.toggle('active');
   });
 });
 
 dropdowns.forEach(dropdown => {
-  dropdown.addEventListener('mouseover', () => {
-    const buttonId = dropdown.id.replace('-dropdown', '');
-    const button = document.getElementById(buttonId);
-
-    dropdown.classList.add('show');
-    button.classList.add('active');
-  });
-
-  dropdown.addEventListener('mouseout', () => {
+  dropdown.addEventListener('click', () => {
     const buttonId = dropdown.id.replace('-dropdown', '');
     const button = document.getElementById(buttonId);
 
@@ -50,9 +24,30 @@ dropdowns.forEach(dropdown => {
 
 window.addEventListener('scroll', () => {
   const navigation = document.querySelector('.navigation');
-  if (window.scrollY > 16) {
+  if (window.scrollY > 8) {
     navigation.classList.add('scrolled');
   } else {
     navigation.classList.remove('scrolled');
+  }
+});
+
+
+// if .navigation .mobile-menu button is clicked toggle the class 'show' on .navigation .menu
+const mobileMenuButton = document.querySelector('.navigation .mobile-menu button');
+const menu = document.querySelector('.navigation .menu');
+
+mobileMenuButton.addEventListener('click', () => {
+  menu.classList.toggle('show');
+  // change text of button
+  if (menu.classList.contains('show')) {
+    mobileMenuButton.innerHTML = `Sluit menu <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 12H21M3 6H21M9 18H21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `;
+  } else {
+    mobileMenuButton.innerHTML = `Open menu <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 12H21M3 6H21M9 18H21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `;
   }
 });
