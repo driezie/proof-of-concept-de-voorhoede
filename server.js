fa71367b65bbc5099e0f74e4cca40239ccf15453
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-06-10 11:25:30 +0200

    Push

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { makeFetchRequest } from './helpers/fetchManager.js';
 
-const app = express();
+const app = express()
 const apiUrl = "https://fdnd-agency.directus.app/items";
 
 // Stel ejs in als de template engine en configureer de views directory
@@ -14,23 +14,18 @@ app.use(express.static('public'));
 // Parseer URL-gecodeerde bodies
 app.use(express.urlencoded({ extended: true }));
 
-
 // Fetch de data van de API
 const fetchFromApi = (endpoint) => {
   return makeFetchRequest(apiUrl + endpoint).then(response => response.json());
 };
 
 app.get('/', (req, res) => {
-  // Render de 'index' template met het huidige thema
+  // Render de 'index' template
   res.render('index');
 });
 
-
-
-
 // Routes om pagina's weer te geven met de thema voorkeur
 const pages = ['vacancies', 'about', 'news', 'members', 'contact', 'cases'];
-
 
 pages.forEach(page => {
   app.get(`/${page}`, (req, res) => {
@@ -44,7 +39,6 @@ pages.forEach(page => {
       return;
     } else if (page === 'members') {
       fetchFromApi('/dda_agencies').then(data => {
-        console.log(data);
         // Render de template voor elke pagina met het huidige thema en de data van de API
         res.render(page, { data: data.data });
       });
@@ -55,7 +49,6 @@ pages.forEach(page => {
   });
 });
 
-
 const detailPages = ['member'];
 
 detailPages.forEach(page => {
@@ -63,12 +56,11 @@ detailPages.forEach(page => {
     app.get(`/${page}/:id`, (req, res) => {
       fetchFromApi('/dda_agencies/' + req.params.id).then(data => {
         // Render the template for each page with the current theme and the data from the API
-        res.render(page, { member: data.data });
+        res.render(page, { member: data.data})
       });
     });
   }
 });
-
 
 // Set the port number
 app.set('port', process.env.PORT || 8000);
